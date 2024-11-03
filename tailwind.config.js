00d8f947af29067931bdc7057e3092c3cfa25670
zolianda <zoliandaam@gmail.com>
2024-11-03 09:31:55 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: { 
+    container:{
+      padding: '16px',
+      center:true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
